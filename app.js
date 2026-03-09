d0212f8dba58acc66b373713744ac24e76911cda
zakyaSharmeen <zakyasharmeen@gmail.com>
2026-03-09 23:15:53 +0530

    added

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 "use strict";
-// interface = defines the structure of an obj
+// type aliases- custom name to type means
+// type ID = string; // now id means a string u can put id instead of string listeraaly oin the dt types name section
 Object.defineProperty(exports, "__esModule", { value: true });
-function abcd(obj) {
-    obj.admin;
+class Device {
+    name = "harsh";
+    price = 1200;
+    category = "digital";
 }
-abcd({ admin: true });
+let d1 = new Device();
+let d2 = new Device();
+console.log(d1);
+console.log(d2);
 //# sourceMappingURL=app.js.map
---
+++ app.ts
@@ -0,0 +1,12 @@
+// type aliases- custom name to type means
+// type ID = string; // now id means a string u can put id instead of string listeraaly oin the dt types name section
+
+class Device {
+  name = "harsh";
+  price = 1200;
+  category = "digital";
+}
+let d1 = new Device();
+let d2 = new Device();
+console.log(d1);
+console.log(d2);
