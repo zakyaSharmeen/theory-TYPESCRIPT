9e57c733f0895865d69a4510c536b9b0acc6b6e7
zakyaSharmeen <zakyasharmeen@gmail.com>
2026-03-09 23:51:25 +0530

    added

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
-"use strict";
 // type aliases- custom name to type means
 // type ID = string; // now id means a string u can put id instead of string listeraaly oin the dt types name section
-Object.defineProperty(exports, "__esModule", { value: true });
-class Device {
-    name = "harsh";
-    price = 1200;
-    category = "digital";
-}
-let d1 = new Device();
-let d2 = new Device();
-console.log(d1);
-console.log(d2);
-//# sourceMappingURL=app.js.map
+// class Device {
+//   name = "harsh";
+//   price = 1200;
+//   category = "digital";
+// }
+// let d1 = new Device();
+// let d2 = new Device();
+// console.log(d1);
+// console.log(d2);
+var BottleMaker = /** @class */ (function () {
+    function BottleMaker(brand, price) {
+        this.brand = brand;
+        this.price = price;
+        console.log("hii crrt");
+    }
+    return BottleMaker;
+}());
+var b1 = new BottleMaker("milton", 1200);
--- app.ts
+++ app.ts
@@ -1,12 +1,22 @@
 // type aliases- custom name to type means
 // type ID = string; // now id means a string u can put id instead of string listeraaly oin the dt types name section
 
-class Device {
-  name = "harsh";
-  price = 1200;
-  category = "digital";
+// class Device {
+//   name = "harsh";
+//   price = 1200;
+//   category = "digital";
+// }
+// let d1 = new Device();
+// let d2 = new Device();
+// console.log(d1);
+// console.log(d2);
+
+class BottleMaker {
+  constructor(
+    public brand: string,
+    public price: number,
+  ) {
+    console.log("hii crrt");
+  }
 }
-let d1 = new Device();
-let d2 = new Device();
-console.log(d1);
-console.log(d2);
+let b1 = new BottleMaker("milton", 1200);
