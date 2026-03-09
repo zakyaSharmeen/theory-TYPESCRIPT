858292e86ed7165c28909578ab8e6c9fedff764a
zakyaSharmeen <zakyasharmeen@gmail.com>
2026-03-09 14:00:32 +0530

    added

---
+++ app.js
@@ -0,0 +1,8 @@
+"use strict";
+// interface = defines the structure of an obj
+Object.defineProperty(exports, "__esModule", { value: true });
+function abcd(obj) {
+    obj.admin;
+}
+abcd({ admin: true });
+//# sourceMappingURL=app.js.map
